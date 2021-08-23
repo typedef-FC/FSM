1b746533bd5aa3179d45a729d671a8e901f42c97
typedef <87053317+typedef-FC@users.noreply.github.com>
2021-08-23 09:44:51 -0400

    1.1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /*
 	@title
-		main.cpp
+		main.h
 
 	@author
 		typedef
@@ -12,8 +12,9 @@
 /// moonlight_information
 #include "include/moonlight/moonlight.h"
 
-/// gui_information + imgui
+/// gui_information + imgui + menu options
 #include "gui/gui.h"
+#include "gui/options.h"
 
 /// logger_information
 #include "include/logger/logger.h"
@@ -24,7 +25,7 @@
 /// std::format
 #include <format>
 
-int wmain( )
+int _stdcall WinMain( HINSTANCE, HINSTANCE, LPSTR, int )
 {
 	try 
 	{
@@ -82,6 +83,21 @@ int wmain( )
 					{
 						if (ImGui::BeginMenu("File"))
 						{
+							if (ImGui::BeginMenu("Profiles"))
+							{
+								/// Loop through all profiles.
+								for (auto& i : g_configuration.profiles)
+								{
+									/// Create radio buttons. Only set active to one that matches the current profile.
+									if (ImGui::RadioButton(i->name.c_str(), !i->name.compare(g_configuration.current_profile)))
+									{
+										/// Load profile if button selected.
+										g_configuration.load_profile(i);
+									}
+								}
+								ImGui::EndMenu();
+							}
+
 							if (ImGui::MenuItem("Exit"))
 							{
 								break;
@@ -92,26 +108,28 @@ int wmain( )
 
 						if (ImGui::BeginMenu("Tools"))
 						{
-							if (ImGui::BeginMenu("Profiles"))
+							/// Loop through all our tools and create a way to open them.
+							for (auto& i : m_tools)
 							{
-								/// Loop through all profiles.
-								for (auto& i : g_configuration.profiles)
+								if (ImGui::MenuItem(i.first))
 								{
-									/// Create radio buttons. Only set active to one that matches the current profile.
-									if (ImGui::RadioButton(i->name.c_str(), !i->name.compare(g_configuration.current_profile)) )
-									{
-										/// Load profile if button selected.
-										g_configuration.load_profile(i);
-									}
+									ShellExecute(NULL, L"open", std::wstring(g_moonlight.directory.wstring() + L"\\resources\\fsm\\tools\\" + i.second).c_str(), NULL, NULL, SW_SHOWNORMAL);
 								}
-								ImGui::EndMenu();
 							}
 
-							if (ImGui::MenuItem("Settings"))
+							ImGui::EndMenu();
+						}
+
+						if (ImGui::BeginMenu("Guides"))
+						{
+							/// Loop through all our guides and create a hyperlink that we can open.
+							for (auto& i : m_guides)
 							{
-								/// Unused for now.
+								if (ImGui::MenuItem(i.first))
+								{
+									ShellExecute(NULL, L"open", i.second, NULL, NULL, SW_SHOWNORMAL);
+								}
 							}
-
 							ImGui::EndMenu();
 						}
 
@@ -127,16 +145,48 @@ int wmain( )
 						/// Set to start at first column.
 						ImGui::TableNextColumn();
 						{
-							ImGui::Text("Script");
+							if (ImGui::Selectable("Script"))
+							{
+								/// Sort by name.
+								std::sort(g_moonlight.script_database.begin(), g_moonlight.script_database.end(), [](const script_information& a, const script_information& b) -> bool
+									{
+										return a.name < b.name;
+									}
+								);
+							}
 							ImGui::TableNextColumn();
 
-							ImGui::Text("Description");
+							if (ImGui::Selectable("Description"))
+							{
+								/// Sort by description.
+								std::sort(g_moonlight.script_database.begin(), g_moonlight.script_database.end(), [](const script_information& a, const script_information& b) -> bool
+									{
+										return a.description < b.description;
+									}
+								);
+							}
 							ImGui::TableNextColumn();
 
-							ImGui::Text("Class");
+							if (ImGui::Selectable("Class"))
+							{
+								/// Sort by script class.
+								std::sort(g_moonlight.script_database.begin(), g_moonlight.script_database.end(), [](const script_information& a, const script_information& b) -> bool
+									{
+										return a.script_class < b.script_class;
+									}
+								);
+							}
 							ImGui::TableNextColumn();
 
-							ImGui::Text("Whitelisted");
+							if (ImGui::Selectable("Whitelisted"))
+							{
+								/// Sort by whitelist.
+								std::sort(g_moonlight.script_database.begin(), g_moonlight.script_database.end(), [](const script_information& a, const script_information& b) -> bool
+									{
+										return a.whitelisted && !b.whitelisted;
+									}
+								);
+							}
 							ImGui::TableNextColumn();
 						}
 
@@ -270,7 +320,9 @@ int wmain( )
 					/// List of actions.
 					static const char* executable_options[] =
 					{
-						"Start Moonlight",
+						"Start Moonlight (Standalone)",
+						"Start Moonlight (CS:GO)",
+						"Start Moonlight (TF2)",
 						"Read Patch Notes",
 						"Open General Logs",
 						"Open Warning Logs",
@@ -292,116 +344,43 @@ int wmain( )
 						{
 							case 0: /// Start Moonlight
 							{
-								/// Save our profile settings.
-								g_configuration.save_profile();
-
-								/// The user didn't set the Moonlight binary path yet. Nothing to execute.
-								if (current_profile->path.empty())
-								{
-									g_logger.add("Missing Moonlight binary path. Please go to Profiles -> Moonlight Binary.");
-									break;
-								}
-
-								/// Update pure_mode.txt with latest profile information. This could be done better***
-								g_moonlight.pure_mode = current_profile->data.pure_mode;
-								g_moonlight.set_pm();
-
-								/*
-									Here we are organizing the scripts.
-
-									1) Create a new directory where we place all the enabled scripts.
-									2) Move the enabled scripts to the root folder.
-									3) Delete the directory.
-								*/
-								auto created_directory = g_moonlight.directory.string() + "fsm";
-								auto scripts_directory = g_moonlight.directory.string() + "\\scripts\\";
-
-								/// #1 - Remove the directory just in case it exists already.
-								std::filesystem::remove_all(created_directory.c_str());
-								if (!std::filesystem::create_directory(created_directory.c_str()))
-								{
-									g_logger.add("Failed to create directory to start Moonlight.");
-									break;
-								}
-
-								/// Move the enabled scripts to our new directory.
-								for (auto& i : g_moonlight.script_database)
-								{
-									/// Only enabled scripts.
-									if (!i.enabled) continue;
-
-									/// Core scripts and library scripts are always on.
-									if (i.path.find("\\core\\") != std::string::npos || i.path.find("\\lib\\") != std::string::npos) continue;
-
-									/// Copy now.
-									std::filesystem::copy(i.path, created_directory + "\\" + i.name);
-								}
-
-								/// #2 - Move the enabled scripts.
-								for (auto& i : std::filesystem::directory_iterator(scripts_directory) )
-								{
-									/// Ignore files without extensions.
-									if (!i.path().has_extension()) continue;
-	
-									/// Copy the files to disabled or else we can permanently lose scripts.
-									std::filesystem::copy(i.path().string(), scripts_directory + "disabled\\", std::filesystem::copy_options::overwrite_existing);
-
-									/// Delete the files already inside of moonlight/scripts.
-									std::filesystem::remove(i.path().string());
-								}
-
-								/// Move our temporarily stored scripts to our root directory.
-								for (auto& i : std::filesystem::directory_iterator(created_directory))
-								{
-									std::filesystem::copy(i.path(), scripts_directory, std::filesystem::copy_options::overwrite_existing);
-								}
-								
-								/// #3 - Delete directory.
-								std::filesystem::remove_all(created_directory.c_str());
-
-								/// Launch Moonlight based on the executable path provided.
-								STARTUPINFOA startup_info = 
-								{ 
-									sizeof(startup_info) 
-								};
-								PROCESS_INFORMATION process_info;
-
-								/// CreateProcess, but launch with "CREATE_NEW_CONSOLE" otherwise it will hijack the current process console if applicable.
-								if (!CreateProcessA(current_profile->path.c_str(), NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &startup_info, &process_info))
-								{
-									g_logger.add("Process could not start.");
-									break;
-								}
-							
-								/*
-									Log and pop up that the process started.
-									TODO: Work like Mod Organizer and disable the GUI instead of a verbal request.
-								*/
-								auto pfmt = std::format("Process started (Process ID: {}). Please don't make further changes in FSM until Moonlight is closed.", process_info.dwProcessId);
-								g_logger.add(pfmt);
-								MessageBoxA(NULL, pfmt.c_str(), "FSM", MB_OK);
+								g_moonlight.start();
 								break;
 							}
 
-							case 1: /// Read Patch Notes
+							case 1: /// Start Moonlight (CS:GO)
+							{
+								ShellExecute(NULL, L"open", L"steam://rungameid/730/", NULL, NULL, SW_SHOWNORMAL);
+								g_moonlight.start();
+								break;
+							}
+
+							case 2: /// Start Moonlight (TF2)
+							{
+								ShellExecute(NULL, L"open", L"steam://rungameid/440/", NULL, NULL, SW_SHOWNORMAL);
+								g_moonlight.start();
+								break;
+							}
+
+							case 3: /// Read Patch Notes
 							{
 								ShellExecute(NULL, L"open", L"https://fantasy.cat/update/index.php?c=Moonlight", NULL, NULL, SW_SHOWNORMAL);
 								break;
 							}
 
-							case 2: /// Open General Logs
+							case 4: /// Open General Logs
 							{
 								ShellExecute(NULL, L"open", std::wstring( g_moonlight.directory.wstring() + L"\\logs\\log.txt" ).c_str(), NULL, NULL, SW_SHOWNORMAL);
 								break;
 							}
 
-							case 3: /// Open Warning Logs
+							case 5: /// Open Warning Logs
 							{
 								ShellExecute(NULL, L"open", std::wstring(g_moonlight.directory.wstring() + L"\\logs\\warning.txt").c_str(), NULL, NULL, SW_SHOWNORMAL);
 								break;
 							}
 
-							case 4: /// Open Error Logs
+							case 6: /// Open Error Logs
 							{
 								ShellExecute(NULL, L"open", std::wstring(g_moonlight.directory.wstring() + L"\\logs\\error.txt").c_str(), NULL, NULL, SW_SHOWNORMAL);
 								break;
@@ -496,9 +475,6 @@ int wmain( )
 								static std::string new_profile_name;
 
 								ImGui::SetCursorPosX(tab_cursor_position);
-								ImGui::Separator();
-
-								ImGui::SetCursorPosX(tab_cursor_position);
 								ImGui::Text("Create new profile:");
 
 								ImGui::SetNextItemWidth(400);
@@ -556,6 +532,12 @@ int wmain( )
 										ImGui::InputText("Description", &i.description);
 										ImGui::Combo("Class", &i.script_class, class_options, IM_ARRAYSIZE(class_options), IM_ARRAYSIZE(class_options));
 
+										if (ImGui::Button("Open in Editor", ImVec2(130, 20)))
+										{
+											/// Open script with default editor.
+											ShellExecuteA(NULL, "open", i.path.c_str(), NULL, NULL, SW_SHOWNORMAL);
+										}
+
 										ImGui::TreePop();
 									}
 									ImGui::EndChild();
@@ -569,6 +551,10 @@ int wmain( )
 								g_moonlight.set_scripts_metadata(g_moonlight.script_database);
 								g_logger.add("Set new script metadata.");
 							}
+
+							
+
+							
 							ImGui::EndTabItem();
 						}
 					}
